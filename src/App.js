5432fabea755c212f8b9ba431811e97620c93606
aparkening <aaron.parkening@gmail.com>
2020-02-27 17:01:41 -0500

    Move navbar out of switch. Comment out auth items.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import {
   Switch
 } from 'react-router-dom';
 import { fetchEvents } from './actions/eventActions'
-import { setupAuth } from './actions/authActions'
+// import { setupAuth } from './actions/authActions'
+
+// import { fetchCats } from './actions/catActions'
+// import CatList from './components/CatList'
 
 import Home from './components/Home';
-import Parks from './components/Parks';
-import Events from './components/Events';
+import ParksContainer from './containers/ParksContainer';
+import EventsContainer from './containers/EventsContainer';
 import Navigation from './components/Navigation';
 
 class App extends Component {
@@ -19,42 +22,63 @@ class App extends Component {
   componentDidMount() {
     // Show initial array (should be empty)
     // console.log(this.props)
-    // this.props.fetchEvents()
-    this.props.setupAuth()
+    this.props.fetchEvents()
+    // this.props.setupAuth()
 
+    // this.props.fetchCats()
   }
 
-  render() {
-    return (
-      <Router>
-        <Switch>
+  // render() {
+  //   return ( 
+  //     <div className="App">
+  //       {/* <h1>CatBook</h1>
+  //       <CatList catPics={this.props.catPics} loading={this.props.loading} /> */}
+  //       <Home />
+  //     </div>
+  //   );
+  // }   
+    render() {
+      return (
+        <Router>
           <Navigation />
-          <Route path='/parks' component={Parks} />
-          <Route path='/events' component={Events} />
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </Router>
-    );
-  }         
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path='/parks' component={ParksContainer} />
+            <Route path='/events' component={EventsContainer} />
+          </Switch>
+        </Router>
+      );
+    }
+  
 }
 
 
 // Make parks, events, and loading available
 const mapStateToProps = state => {
+  // return {
+  //   catPics: state.cats,
+  //   loading: state.loading
+  // }
   return {
-    parks: state.parks,
     events: state.events,
-    loading: state.loading,
-    csrf: state.csrf
+    loading: state.loading
   }
 }
+// parks: state.parks,
+// events: state.events,
+// loading: state.loading,
+// csrf: state.csrf
+
  
 // Make fetchEvents() available on mount
 const mapDispatchToProps = dispatch => {
+  // return {
+  //   fetchCats: () => dispatch(fetchCats())
+  // }
   return {
-    fetchEvents: () => dispatch(fetchEvents()),
-    setupAuth: () => dispatch(setupAuth())
+    fetchEvents: () => dispatch(fetchEvents())
   }
 }
+// , setupAuth: () => dispatch(setupAuth())
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
