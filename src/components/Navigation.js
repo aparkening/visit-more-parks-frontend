952130d39714b789bc27f32d080096d610f53059
aparkening <aaron.parkening@gmail.com>
2020-02-27 14:06:43 -0500

    Add Events and Parks links.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,15 @@
+import React, { Component } from 'react';
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 class Navigation extends Component {
   render() {
     return (
       <Navbar bg="primary" expand="true" role="navigation">
-        <Navbar.Brand href="#home">Visit More Parks</Navbar.Brand>
+        <Navbar.Brand href="/">Visit More Parks</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+          <Nav.Link href="/events">Events</Nav.Link>
+          <Nav.Link href="/parks">Parks</Nav.Link>
         </Nav>
       </Navbar>
     )
