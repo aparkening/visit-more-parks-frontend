03817d13fec8fd8643fd581cc3d55fde379ad8fc
aparkening <aaron.parkening@gmail.com>
2020-02-27 09:52:50 -0500

    Add rootReducer.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.scss';
 import './index.css';
 
-import App from './App';
-
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
-import reducers from './reducers/';
-
-const store = createStore(reducers, applyMiddleware(thunk))
+const store = createStore(rootreducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
