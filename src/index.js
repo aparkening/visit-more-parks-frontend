0fc47fd0c3a63a85205eba906f15ce8d318a8b82
aparkening <aaron.parkening@gmail.com>
2020-02-27 17:00:30 -0500

    Test redux with eventsReducer.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom'
+import App from './App'
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.scss';
 import './index.css';
 
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+// import rootReducer from './reducers';
+import eventsReducer from './reducers/eventsReducer';
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(eventsReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
