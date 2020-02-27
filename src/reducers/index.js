ed168611e1c8c8c529a0f45f3f189e8fdc99b049
aparkening <aaron.parkening@gmail.com>
2020-02-27 11:58:34 -0500

    Shorten combineReducers; add comments for later.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,15 @@
 import { combineReducers } from "redux";
+import * as reducers from './reducers';
+
+// As separate files
+// import eventsReducer from './reducers/eventsReducer';
+// import parksReducer from './reducers/parksReducer';
  
-export default combineReducers({
-  // parks,
-  // events
-});
+export default combineReducers(reducers);
+
+// As separate fields
+// const rootReducer = combineReducers({
+//   events: eventsReducer,
+//   parks: parksReducer
+// })
+// export default rootReducer;
