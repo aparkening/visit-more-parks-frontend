b917e4e9925fc95713bcbfc9fa203e3cb08a4a0c
aparkening <aaron.parkening@gmail.com>
2020-02-27 14:58:47 -0500

    Update type.

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -3,6 +3,6 @@ export const setupAuth = () => {
     dispatch({ type: 'GETTING_AUTHORIZATION' });
     fetch('http://localhost:3000/auth-check',{credentials: 'include'})
       .then(response => response.json())
-      .then(responseJSON => dispatch({ type: 'ADD_AUTH', csrf: responseJSON.csrf_auth_token }));
+      .then(responseJSON => dispatch({ type: 'ADD_AUTHORIZATION', csrf: responseJSON.csrf_auth_token }));
   };
 }
