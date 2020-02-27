ae675d97cfb957411d65c9626f312924cc1fdc39
aparkening <aaron.parkening@gmail.com>
2020-02-27 17:03:10 -0500

    Reduce output to text.

--- src/containers/ParksContainer.js
+++ src/containers/ParksContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Parks from './components/Parks'
+import Parks from '../components/Parks'
 
-class App extends Component {
+class ParksContainer extends Component {
   constructor(){
     super()
     this.state = {
@@ -9,45 +9,45 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-    fetch("http://localhost:3000/parks")
-      .then(r=>r.json())
-      .then(parks => this.setState({parks: parks}))
-  }
+  // componentDidMount() {
+  //   fetch("http://localhost:3000/parks")
+  //     .then(r=>r.json())
+  //     .then(parks => this.setState({parks: parks}))
+  // }
 
-  handleLikeClick = (event) => {
-    let newStatus
-    if (event.target.tagName === "IMG") {newStatus = "undecided"}
-    else {newStatus =  event.target.className === "fav-button" ? "favorited" : ""}
-    const parks = this.state.parks.map(park => {
-      if (parseInt(parks.id) === parseInt(event.target.id) || park.id == event.target.className) {
-        const newPark = Object.assign({}, park)
-        newPark.status = newStatus
-        return newPark
-      } else {
-        return park
-      }
-    })
-    this.setState({
-      parks: parks
-    })
-  }
+  // handleLikeClick = (event) => {
+  //   let newStatus
+  //   if (event.target.tagName === "IMG") {newStatus = "undecided"}
+  //   else {newStatus =  event.target.className === "fav-button" ? "favorited" : ""}
+  //   const parks = this.state.parks.map(park => {
+  //     if (parseInt(parks.id) === parseInt(event.target.id) || park.id == event.target.className) {
+  //       const newPark = Object.assign({}, park)
+  //       newPark.status = newStatus
+  //       return newPark
+  //     } else {
+  //       return park
+  //     }
+  //   })
+  //   this.setState({
+  //     parks: parks
+  //   })
+  // }
 
   render() {
 
     return (
-      <div className="App">
+      <div className="Parks">
         <Parks
           handleChangeOfHeart={this.handleFavClick}
           parks={this.state.parks.filter(park => park.status === "favorited")}
         />
         <Parks
           handleChangeOfHeart={this.handleFavClick}
-          parks={this.state.parks.filter(park => park.status === "undecided")}
+          parks={this.state.parks}
         />
       </div>
     );
   }
 }
 
-export default App;
+export default ParksContainer;
