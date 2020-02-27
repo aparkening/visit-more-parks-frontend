427be29e55a016f672a840bfc2d03230cb24d223
aparkening <aaron.parkening@gmail.com>
2020-02-27 14:56:22 -0500

    Initial commit.

---
+++ src/actions/authActions.js
@@ -0,0 +1,8 @@
+export const setupAuth = () => {
+  return (dispatch) => {
+    dispatch({ type: 'GETTING_AUTHORIZATION' });
+    fetch('http://localhost:3000/auth-check',{credentials: 'include'})
+      .then(response => response.json())
+      .then(responseJSON => dispatch({ type: 'ADD_AUTH', csrf: responseJSON.csrf_auth_token }));
+  };
+}
---
+++ src/actions/eventActions.js
@@ -0,0 +1,43 @@
+export const fetchEvents = () => {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_EVENTS' });
+    const res = await fetch('http://localhost:3000/api/v1/events', {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-CSRF-TOKEN': this.state.csrf
+      },
+      credentials: 'include'
+    })
+    const data = await res.json()
+    dispatch({ type: 'ADD_EVENTS', events: data });
+
+    // Add try/catch once working
+    // try {
+    //   const res = await fetch('http://localhost:3000/api/v1/events', {
+    //     headers: {
+    //       'Accept': 'application/json',
+    //       'Content-Type': 'application/json',
+    //       'X-CSRF-TOKEN': this.state.csrf
+    //     },
+    //     credentials: 'include'
+    //   })
+    //   if (!res.ok){ throw res; }
+    //   const data = await res.json()
+    //   dispatch({ type: 'ADD_EVENTS', events: data });
+    // } catch (err) {
+    //   console.log("Error happened!")
+    // }
+
+    // fetch('http://localhost:3000/api/v1/events', {
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json',
+    //     'X-CSRF-TOKEN': this.state.csrf
+    //   },
+    //   credentials: 'include'
+    // })
+    //   .then(response => response.json())
+    //   .then(responseJSON => dispatch({ type: 'ADD_EVENTS', events: responseJSON }));
+  };
+}
