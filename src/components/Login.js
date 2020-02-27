a2ef85888f5bb2bab63384a97daa300f73eb2445
aparkening <aaron.parkening@gmail.com>
2020-02-27 14:10:31 -0500

    Iniital commit.

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+ 
+const Login = () => {
+  <Button href="http://localhost:3000/auth/google/redirect" variant="primary">Sign in with Google</Button>
+}
+
+// class Login extends React.Component {
+
+//   render() {
+//     return (
+      
+//     );
+//   }
+// }
+ 
+export default Login;
