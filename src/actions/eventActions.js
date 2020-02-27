2c45bca0b6b24be64999ccc8a7fb54dc0e13a88b
aparkening <aaron.parkening@gmail.com>
2020-02-27 17:04:39 -0500

    Reduce output to text.

--- src/actions/eventActions.js
+++ src/actions/eventActions.js
@@ -1,16 +1,32 @@
 export const fetchEvents = () => {
   return (dispatch) => {
     dispatch({ type: 'LOADING_EVENTS' });
-    const res = await fetch('http://localhost:3000/api/v1/events', {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'X-CSRF-TOKEN': this.state.csrf
-      },
-      credentials: 'include'
-    })
-    const data = await res.json()
-    dispatch({ type: 'ADD_EVENTS', events: data });
+    console.log("Events are loading")
+    dispatch({type: 'ADD_EVENTS', events: ['event 1', 'event 2']})
+
+    // fetch('http://localhost:3000/api/v1/events', {
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json',
+    //     'X-CSRF-TOKEN': this.state.csrf
+    //   },
+    //   credentials: 'include'
+    // })
+    //   .then(response => response.json())
+    //   .then(responseJSON => dispatch({ type: 'ADD_EVENTS', events: responseJSON }));
+
+
+
+    // const res = await fetch('http://localhost:3000/api/v1/events', {
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json',
+    //     'X-CSRF-TOKEN': this.state.csrf
+    //   },
+    //   credentials: 'include'
+    // })
+    // const data = await res.json()
+    // dispatch({ type: 'ADD_EVENTS', events: data });
 
     // Add try/catch once working
     // try {
@@ -29,15 +45,6 @@ export const fetchEvents = () => {
     //   console.log("Error happened!")
     // }
 
-    // fetch('http://localhost:3000/api/v1/events', {
-    //   headers: {
-    //     'Accept': 'application/json',
-    //     'Content-Type': 'application/json',
-    //     'X-CSRF-TOKEN': this.state.csrf
-    //   },
-    //   credentials: 'include'
-    // })
-    //   .then(response => response.json())
-    //   .then(responseJSON => dispatch({ type: 'ADD_EVENTS', events: responseJSON }));
+
   };
 }
