f6fc200fe95eeecf9be86c1de354a046821819bf
aparkening <aaron.parkening@gmail.com>
2020-02-28 16:49:46 -0500

    Add test login field to test props.active.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,6 +3,9 @@ import { Navbar, Nav } from "react-bootstrap";
 
 class Navigation extends Component {
   render() {
+  
+    console.log(this.props)
+  
     return (
       <Navbar className="justify-content-center" bg="coffee" variant="dark" expand role="navigation">
         <Navbar.Brand href="/">Visit More Parks</Navbar.Brand>
@@ -16,6 +19,16 @@ class Navigation extends Component {
           <Nav.Item>
             <Nav.Link href="/parks" activeclassname="active">Parks</Nav.Link>
           </Nav.Item>
+
+
+          <Nav.Item>
+            {this.props.active ? <Nav.Link onClick={() => this.props.removeAuth(this.props.token)} >Logout</Nav.Link> :  <Nav.Link href="http://localhost:3000/auth/google/redirect">Login with Google</Nav.Link>}
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link href="http://localhost:3000/auth/google/redirect">Login with Google</Nav.Link>
+          </Nav.Item>
+
         </Nav>
       </Navbar>
     )
