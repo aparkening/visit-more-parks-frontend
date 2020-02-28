4a7b5a8736935959aa35e17810fc591fa75b11f6
aparkening <aaron.parkening@gmail.com>
2020-02-28 16:47:17 -0500

    Add token and active to test.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
  
-
 class Login extends Component {
-// const Login = () => {
   render() {
     return(
       <div>
         <Button href="http://localhost:3000/auth/google/redirect" variant="copper">Sign in with Google</Button>
-        {/* <p>{this.props.token}</p> */}
+        <p>{this.props.token}</p>
+        Active: {this.props.active? "Yes" : "No"}
       </div>
     )
   }
 }
-
-// class Login extends React.Component {
-
-//   render() {
-//     return (
-      
-//     );
-//   }
-// }
  
 export default Login;
