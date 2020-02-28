72f8f96810ce26edba85df686232ce513523d952
aparkening <aaron.parkening@gmail.com>
2020-02-28 16:45:40 -0500

    Add removeAuth with token parameter.

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,4 +1,4 @@
-export const setupAuth = () => {
+const setupAuth = () => {
   return (dispatch) => {
     dispatch({ type: 'GETTING_AUTHORIZATION' });
     fetch('http://localhost:3000/auth-check',{credentials: 'include'})
@@ -6,3 +6,24 @@ export const setupAuth = () => {
       .then(responseJSON => dispatch({ type: 'ADD_AUTHORIZATION', csrf: responseJSON.csrf_auth_token }));
   };
 }
+
+const removeAuth = (token) => {
+  const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'X-CSRF-TOKEN': token
+  }
+  console.log(headers)
+  return (dispatch) => {
+    dispatch({ type: 'REMOVING_AUTHORIZATION' });
+    fetch('http://localhost:3000/logout',{
+      method: "DELETE",
+      headers,
+      credentials: 'include'
+    })   
+    .then(response => response.json())
+    .then(responseJSON => dispatch({ type: 'LOG_OUT'}));
+  };
+}
+
+export {setupAuth, removeAuth}
