c6e9e93d110b29d7b7ffecdf14de229ff3d068fc
aparkening <aaron.parkening@gmail.com>
2020-02-29 00:18:10 -0500

    Add loading props.

--- src/components/Event.js
+++ src/components/Event.js
@@ -3,13 +3,25 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Badge from 'react-bootstrap/Badge';
 import Park from './Park';
 
 const Event = props => {
   const { event } = props;
-  console.log(props)
+  console.log(props);
 
-  const parks = event.nearParks.map(obj => <Col sm={6}><Park key={obj.id} park={obj} /></Col>)
+  const addTrip = (id) => {
+    console.log("Park id:", id)
+  };
+
+  const parks = event.nearParks.map(obj => {
+    return (
+      <Col sm={6} key={obj.id}>
+        <Badge variant="coffee" onClick={addTrip(obj.id)}>Add Park to Trip!</Badge>
+        <Park park={obj} />
+      </Col>
+    );
+  });
 
   if (props.loading) {
     return (<div className="loader"></div>)
@@ -21,10 +33,11 @@ const Event = props => {
             <Card.Text>{event.location}</Card.Text>
             <Card.Text>{event.start.date ? event.start.date : event.start.dateTime} - {event.end.date ? event.end.date : event.end.dateTime} {event.end.timeZone? (event.end.timeZone) : ''}</Card.Text>
             <Card.Text>{event.description}</Card.Text>
-            <Button variant="copper" size="sm" className="mr-2" onClick={() => props.updateEvent(event.id)}> Edit </Button>
-            <Button variant="copper" size="sm" className="mr-2" onClick={() => props.deleteEvent(event.id)}> X </Button>
+            {/* <Button variant="copper" size="sm" className="mr-2" onClick={() => props.updateEvent(event.id)}> Edit </Button>
+            <Button variant="copper" size="sm" className="mr-2" onClick={() => props.deleteEvent(event.id)}> X </Button> */}
+    
+            <h4 className="mt-4">Nearby Parks</h4>
             <Row>
-              <h4>Nearby Parks</h4>
               {parks}
             </Row>
           </Card.Body>
