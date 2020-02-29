187036d068cffef955e6bcc3a169116ee8031f7d
aparkening <aaron.parkening@gmail.com>
2020-02-28 23:54:02 -0500

    Add Container and Row components for bootstrap formatting.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 import ParksContainer from '../containers/ParksContainer'
-// import EventsContainer from '../containers/EventContainer'
+import EventsContainer from '../containers/EventsContainer'
 
 const Dashboard = (props) => (
   <div className="container">
     <h1>Dashboard</h1>
     {props.loading ? <div className="loader"></div> : 
-    <ParksContainer token={props.token} />}
-
-    {/* <EventsContainer token={this.props.token} /> */}
-
+    <Container>
+      <Row><EventsContainer token={props.token} /></Row>
+      <Row><ParksContainer token={props.token} /></Row>
+    </Container> }
   </div>
 )
 export default Dashboard
