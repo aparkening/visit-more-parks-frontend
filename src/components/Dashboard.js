a25a8d76b6bbfba4363c20812c4c507e6d07c378
aparkening <aaron.parkening@gmail.com>
2020-02-28 22:41:07 -0500

    Add ParksContainer back in.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,12 +4,10 @@ import ParksContainer from '../containers/ParksContainer'
 
 const Dashboard = (props) => (
   <div className="container">
-    Active: {props.active? "Yes" : "No"}
-
-
     <h1>Dashboard</h1>
-    {props.loading ? <div className="loader"></div> : ""}
-    <ParksContainer token={this.props.token} />
+    {props.loading ? <div className="loader"></div> : 
+    <ParksContainer token={props.token} />}
+
     {/* <EventsContainer token={this.props.token} /> */}
 
   </div>
