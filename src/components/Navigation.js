c4fdff2725a0ed1d80c465c59694eb31e4137fa8
aparkening <aaron.parkening@gmail.com>
2020-02-28 21:29:54 -0500

    Rearrange Nav and NavItem components for better display.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,17 +9,25 @@ class Navigation extends Component {
     return (
       <Navbar className="justify-content-center" bg="coffee" variant="dark" expand role="navigation">
         <Navbar.Brand href="/">Visit More Parks</Navbar.Brand>
-        <Nav>
-          {this.props.activeUser ? 
+        {this.props.token ? 
+          <Nav>
             <Nav.Item>
               <Nav.Link href="/" activeclassname="active">Home</Nav.Link>
-              <Nav.Link href="/events" activeclassname="active">Events</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/events" activeclassname="active">Events</Nav.Link>  
+            </Nav.Item>
+            <Nav.Item>
               <Nav.Link href="/parks" activeclassname="active">Parks</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
               <Nav.Link onClick={() => this.props.removeAuth(this.props.token)} >Logout</Nav.Link>
-            </Nav.Item> : 
+            </Nav.Item>
+          </Nav> : 
+          <Nav>
             <Nav.Item><Nav.Link href="http://localhost:3000/auth/google/redirect">Login with Google</Nav.Link></Nav.Item>
-          }
-        </Nav>
+          </Nav>
+        }
       </Navbar>
     )
   }
