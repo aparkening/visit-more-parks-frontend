1c70cac9fc4d51a2fedefebd1c30fa4bc33a0f0a
aparkening <aaron.parkening@gmail.com>
2020-02-28 23:11:30 -0500

    Add idx back into events.

--- src/reducers/eventsReducer.js
+++ src/reducers/eventsReducer.js
@@ -1,49 +1,89 @@
-const eventsReducer = (state = { events: [], loading: false }, action) => {
-  let idx;
-  switch(action.type) {
-    case 'LOADING_EVENTS':
-      console.log("Loading events")
-      return {
-        ...state,
-        events: [...state.events],
-        loading: true
-      }
-    case 'ADD_EVENTS':
-      console.log("Add events")
-      return {
-        ...state,
-        events: action.events,
-        loading: false
-      }
-    case 'DELETING_EVENT':
-      return {
-        ...state,
-        events: [...state.events],
-        loading: true
-      }
-    case 'DELETE_EVENT':
-      return {
-        ...state,
-        events: [...state.events.slice(0, idx), ...state.events.slice(idx + 1)],
-        loading: false
-      }
-    case 'UPDATING_EVENT':
-      return {
-        ...state,
-        events: [...state.events],
-        loading: true
-      }
-    case 'UPDATE_EVENT':
-      return {
-        ...state,
-        events: [...state.events.slice(0, idx), action.event, ...state.events.slice(idx + 1)],
-        loading: false
-      }
-
-
+export const eventsErrored = (state = false, action) => {
+  switch (action.type) {
+    case 'EVENTS_ERRORED':
+      return action.hasErrored;
     default:
       return state;
   }
 }
+
+export const eventsLoading = (state = false, action) => {
+  switch(action.type) {
+    case 'EVENTS_LOADING':
+      return action.isLoading;
+    default:
+      return state;
+  }
+}
+
+export const events = (state = [], action) => {
+  let idx;
+  switch (action.type) {
+    case 'ADD_EVENTS':
+      return action.events;
+    case 'DELETE_EVENT':
+      return {
+        ...state,
+        events: [...state.events.slice(0, idx), ...state.events.slice(idx + 1)],
+      }
+    case 'UPDATE_EVENT':
+    return {
+      ...state,
+      events: [...state.events.slice(0, idx), action.event, ...state.events.slice(idx + 1)],
+    }
+    default:
+      return state;
+  }
+}
+
+
+
+// const eventsReducer = (state = { events: [], loading: false }, action) => {
+//   let idx;
+//   switch(action.type) {
+//     case 'LOADING_EVENTS':
+//       console.log("Loading events")
+//       return {
+//         ...state,
+//         events: [...state.events],
+//         loading: true
+//       }
+//     case 'ADD_EVENTS':
+//       console.log("Add events")
+//       return {
+//         ...state,
+//         events: action.events,
+//         loading: false
+//       }
+//     case 'DELETING_EVENT':
+//       return {
+//         ...state,
+//         events: [...state.events],
+//         loading: true
+//       }
+//     case 'DELETE_EVENT':
+//       return {
+//         ...state,
+//         events: [...state.events.slice(0, idx), ...state.events.slice(idx + 1)],
+//         loading: false
+//       }
+//     case 'UPDATING_EVENT':
+//       return {
+//         ...state,
+//         events: [...state.events],
+//         loading: true
+//       }
+//     case 'UPDATE_EVENT':
+//       return {
+//         ...state,
+//         events: [...state.events.slice(0, idx), action.event, ...state.events.slice(idx + 1)],
+//         loading: false
+//       }
+
+
+//     default:
+//       return state;
+//   }
+// }
  
-export default eventsReducer;
+// export default eventsReducer;
