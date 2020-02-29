b72ea451941e5a08db0e6c803dba9a74d0cfd47d
aparkening <aaron.parkening@gmail.com>
2020-02-28 19:22:57 -0500

    Add token to ParksContainer and EventsContainer.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import ParksContainer from '../containers/ParksContainer'
+// import EventsContainer from '../containers/EventContainer'
 
 const Dashboard = (props) => (
   <div className="container">
+    Active: {props.active? "Yes" : "No"}
+
+
     <h1>Dashboard</h1>
     {props.loading ? <div className="loader"></div> : ""}
+    <ParksContainer token={this.props.token} />
+    {/* <EventsContainer token={this.props.token} /> */}
 
-    Active: {props.active? "Yes" : "No"}
   </div>
 )
 export default Dashboard
