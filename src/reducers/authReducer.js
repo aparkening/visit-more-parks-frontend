2adbfaf0d869cbb2b83b0f11dc927b6f26544158
aparkening <aaron.parkening@gmail.com>
2020-02-28 21:31:11 -0500

    Add methods for error and getting authorization. Rename main reducer to csrf.

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,33 +1,57 @@
-const authReducer = (state = { csrf: "", activeUser: false, loading: false }, action) => {
+export const authErrored = (state = false, action) => {
+  switch (action.type) {
+    case 'AUTH_ERRORED':
+      console.log("Error Action")
+      console.log(action)
+      return action.hasErrored;
+    default:
+      return state;
+  }
+}
+
+export const authLoading = (state = false, action) => {
   switch(action.type) {
     case 'GETTING_AUTHORIZATION':
-      return {
-        ...state,
-        csrf: state.csrf,
-        loading: true
-      }
+      console.log("Getting Action")
+      console.log(action)
+      return action.isLoading;
+    default:
+      return state;
+  }
+}
+
+// const authReducer = (state = { csrf: "", activeUser: false, loading: false }, action) => {
+
+export const csrf = (state = false, action) => {
+
+  switch(action.type) {
+    // case 'GETTING_AUTHORIZATION':
+    //   return {
+    //     ...state,
+    //     csrf: state.csrf,
+    //     loading: true
+    //   }
     case 'ADD_AUTHORIZATION':
-      return {
-        ...state,
-        csrf: action.csrf,
-        loading: false,
-        activeUser: true
-      }
-    case 'REMOVING_AUTHORIZATION':
-      return {
-        ...state,
-        csrf: state.csrf,
-        loading: true
-      }      
+      console.log(action);
+      return action.csrf;
+
+    // case 'REMOVING_AUTHORIZATION':
+    //   return {
+    //     ...state,
+    //     csrf: state.csrf,
+    //     loading: true
+    //   }      
     case 'LOG_OUT':
-      return {
-        ...state,
-        loading: false,
-        activeUser: false
-      }      
+      // return {
+      //   ...state,
+      //   loading: false,
+      //   activeUser: false
+      // }      
+      console.log(action);
+      return action.csrf;
     default:
       return state;
   }
 }
  
-export default authReducer;
+// export default authReducer;
