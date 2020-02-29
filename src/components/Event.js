f8839acf0043d236c7b1725c983cacf4a632b777
aparkening <aaron.parkening@gmail.com>
2020-02-28 22:49:55 -0500

    Add initial bootstrap styling.

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,17 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Park from './Park';
 
-class Event extends Component {
-  render() {
-    const { event } = this.props;
+const Event = props => {
+  const { event } = props;
+  console.log(props)
+
+  const parks = props.parks.map(obj => <Park key={obj.id} park={obj} favoritePark={props.favoritePark} unFavoritePark={props.unFavoritePark} loading={props.loading} />)
+
+  if (props.loading) {
+    return (<div className="loader"></div>)
+  } else {
     return (
-      <div>
-        <li>
-          {event}
-          <button onClick={() => this.props.updateEvent(event.id)}> Edit </button>
-          <button onClick={() => this.props.deleteEvent(event.id)}> X </button>
-        </li>
-      </div>
-    );
+      <Card className="event mb-4" bg="white" border="coffee">
+        <Card.Header as="h4">{event.title}</Card.Header>
+        <Card.Body>
+          <Card.Text>{event.location}</Card.Text>
+          <Card.Text>{event.start_time} - {event.end_time} (event.timezone)</Card.Text>
+          <Card.Text>{event.description}</Card.Text>
+          <Button variant="copper" size="sm" onClick={() => props.updateEvent(event.id)}> Edit </Button>
+          <Button variant="copper" size="sm" onClick={() => props.deleteEvent(event.id)}> X </Button>
+          {parks}
+        </Card.Body>
+      </Card>
+    )
   }
 };
 
