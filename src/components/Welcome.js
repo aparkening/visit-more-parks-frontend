bddea150a10fd1dd3c259a54a85c8d597132db5a
aparkening <aaron.parkening@gmail.com>
2020-02-29 00:17:41 -0500

    Add welcome message and login button.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container'
+
 
 const Welcome = () => (
-  <div>
-    <h1>Welcome, let's Visit More Parks!</h1>
-  </div>
+  <Container>
+    <h1>Let's visit more parks!</h1>
+    <p>
+      When you log in with your Google account, Visit More Parks will scan your calendar for upcoming events with locations and suggest nearby U.S. National Parks (within 100 miles).
+    </p>
+    <p>
+      <Button variant="coffee" href="http://localhost:3000/auth/google/redirect">Log in to Google</Button>
+    </p>
+  </Container>
 )
 export default Welcome
