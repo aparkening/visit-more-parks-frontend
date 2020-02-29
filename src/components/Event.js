eb337441412985e33a1451249861e155ecb0bfff
aparkening <aaron.parkening@gmail.com>
2020-02-28 23:46:12 -0500

    Add more bootstrap formatting.

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Card';
 import Park from './Park';
 
 const Event = props => {
   const { event } = props;
   console.log(props)
 
-  const parks = props.parks.map(obj => <Park key={obj.id} park={obj} favoritePark={props.favoritePark} unFavoritePark={props.unFavoritePark} loading={props.loading} />)
+  // const parks = props.nearParks.map(obj => <Park key={obj.id} park={obj} />)
 
   if (props.loading) {
     return (<div className="loader"></div>)
   } else {
     return (
-      <Card className="event mb-4" bg="white" border="coffee">
-        <Card.Header as="h4">{event.title}</Card.Header>
-        <Card.Body>
-          <Card.Text>{event.location}</Card.Text>
-          <Card.Text>{event.start_time} - {event.end_time} (event.timezone)</Card.Text>
-          <Card.Text>{event.description}</Card.Text>
-          <Button variant="copper" size="sm" onClick={() => props.updateEvent(event.id)}> Edit </Button>
-          <Button variant="copper" size="sm" onClick={() => props.deleteEvent(event.id)}> X </Button>
-          {parks}
-        </Card.Body>
-      </Card>
+        <Card className="event mb-4" bg="white" border="coffee">
+          <Card.Header as="h4"><Card.Link href={event.htmlLink}>{event.summary}</Card.Link></Card.Header>
+          <Card.Body>
+            <Card.Text>{event.location}</Card.Text>
+            <Card.Text>{event.start.date ? event.start.date : event.start.dateTime} - {event.end.date ? event.end.date : event.end.dateTime} {event.end.timeZone? (event.end.timeZone) : ''}</Card.Text>
+            <Card.Text>{event.description}</Card.Text>
+            <Button variant="copper" size="sm" className="mr-2" onClick={() => props.updateEvent(event.id)}> Edit </Button>
+            <Button variant="copper" size="sm" className="mr-2" onClick={() => props.deleteEvent(event.id)}> X </Button>
+            {console.log(props.nearParks)}
+          </Card.Body>
+        </Card>
     )
   }
 };
--- src/components/Events.js
+++ src/components/Events.js
@@ -5,13 +5,17 @@ const Events = props => {
   console.log("Events")
   console.log(props)
 
-  const events = props.events.map(obj => <Event key={obj.id} event={obj} deleteEvent={props.deleteEvent} updateEvent={props.updateEvent} />)
+  const events = props.events.map(obj => <Event key={obj.id} event={obj} deleteEvent={props.deleteEvent} updateEvent={props.updateEvent} loading={props.loading} />)
 
-  return (
-    <div className="events">
-      {events}
-    </div>
-  )
+  if (props.loading) {
+    return (<div className="loader"></div>)
+  } else {
+    return (
+      <div className="events">
+        {events}
+      </div>
+    )
+  }
 };
 
 export default Events;
