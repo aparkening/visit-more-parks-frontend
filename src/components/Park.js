de43d9729661a2b0329f4f4d7999d8dae409ef3a
aparkening <aaron.parkening@gmail.com>
2020-02-28 23:30:05 -0500

    Comment out props.landing conditional. Place favorite behind props.favoritePark conditional.

--- src/components/Park.js
+++ src/components/Park.js
@@ -8,21 +8,19 @@ const Park = props => {
   // console.log(props)  
   {/* Add ternary for unfavorite <Button variant="outline-copper" size="sm" onClick={() => this.props.unFavoritePark(park.id)}>Unfavorite</Button> */}
 
-  if (props.loading) {
-    return (<div className="loader"></div>)
-  } else {
+  // if (props.loading) {
+  //   return (<div className="loader"></div>)
+  // } else {
     return (
       <Card className="park mb-4" bg="white" border="coffee">
         <Card.Header as="h4"><Card.Link href={park.url}>{park.fullName}</Card.Link></Card.Header>
         <Card.Body>
-          <Card.Text>{park.description}
-            
-          </Card.Text>
-          <Button variant="copper" size="sm" onClick={() => props.favoritePark(park.id)}>Favorite!</Button>
+          <Card.Text>{park.description}</Card.Text>
+          {props.favoritePark ? <Button variant="copper" size="sm" onClick={() => props.favoritePark(park.id)}>Favorite!</Button> : ''}
         </Card.Body>
       </Card>
     )
-  }
+  // }
 };
 
 export default Park;
