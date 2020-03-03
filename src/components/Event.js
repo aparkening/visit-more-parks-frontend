0957c45ea5a766a0a66813f9213242989e389113
aparkening <aaron.parkening@gmail.com>
2020-03-03 18:40:01 -0500

    Add token and addEvent props to Park component. Rename obj to p in parks map.

--- src/components/Event.js
+++ src/components/Event.js
@@ -8,20 +8,24 @@ import Park from './Park';
 
 const Event = props => {
   const { event } = props;
+  console.log("One Event");
   console.log(props);
+
+// debugger
 
   const addTrip = (id) => {
     console.log("Park id:", id)
   };
 
-  const parks = event.nearParks.map(obj => {
+  const parks = event.nearParks ? event.nearParks.map(p => {
     return (
-      <Col sm={6} key={obj.id}>
-        <Badge variant="coffee" onClick={addTrip(obj.id)}>Add Park to Trip!</Badge>
-        <Park park={obj} />
+      <Col sm={6} key={p.id}>
+        {/* <Badge variant="coffee" onClick={addTrip(obj.id)}>Add Park to Trip!</Badge> */}
+
+        <Park park={p} token={props.token} addEvent={props.addEvent} />
       </Col>
     );
-  });
+  }) : '';
 
 // Add event input form to Event
 //   <EventInput 
@@ -37,11 +41,16 @@ const Event = props => {
         <Card className="event mb-4" bg="white" border="coffee">
           <Card.Header as="h4">{event.summary}</Card.Header>
           <Card.Body>
-            <Card.Text>{event.location}</Card.Text>
-            <Card.Text>{event.start.date ? event.start.date : event.start.dateTime} - {event.end.date ? event.end.date : event.end.dateTime} {event.end.timeZone? (event.end.timeZone) : ''}</Card.Text>
+            <Card.Text>
+              {event.location} <br />
+              {event.start.date ? event.start.date : event.start.dateTime} to {event.end.date ? event.end.date : event.end.dateTime} {event.end.timeZone? (event.end.timeZone) : ''}</Card.Text>
             <Card.Text>{event.description}</Card.Text>
-            {/* <Button variant="copper" size="sm" className="mr-2" onClick={() => props.updateEvent(event.id)}> Edit </Button>
-            <Button variant="copper" size="sm" className="mr-2" onClick={() => props.deleteEvent(event.id)}> X </Button> */}
+
+
+
+
+
+            {/* <Button variant="copper" size="sm" className="mr-2" onClick={() => props.deleteEvent(event.id)}> X </Button> */} */}
     
             <h4 className="mt-4">Nearby Parks</h4>
             <Row>
