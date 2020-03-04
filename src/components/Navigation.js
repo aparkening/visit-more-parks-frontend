203d72d3b88af3fc3f25ecf3fcf2572cbe63e6b4
aparkening <aaron.parkening@gmail.com>
2020-03-03 21:12:09 -0500

    Remove Home and link from brand.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,17 +3,11 @@ import { Navbar, Nav } from "react-bootstrap";
 
 class Navigation extends Component {
   render() {
-  
-    // console.log(this.props)
-  
     return (
-      <Navbar className="justify-content-center" bg="coffee" variant="dark" expand role="navigation">
-        <Navbar.Brand href="/">Visit More Parks</Navbar.Brand>
+      <Navbar className="justify-content-center" bg="secondary" variant="dark" expand role="navigation">
+        <Navbar.Brand>Visit More Parks</Navbar.Brand>
         {this.props.token ? 
           <Nav>
-            <Nav.Item>
-              <Nav.Link href="/" activeclassname="active">Home</Nav.Link>
-            </Nav.Item>
             <Nav.Item>
               <Nav.Link href="/events" activeclassname="active">Events</Nav.Link>  
             </Nav.Item>
