1f53db28832e3e73f78a4d49f47b9269ad165ae0
aparkening <aaron.parkening@gmail.com>
2020-03-03 20:46:35 -0500

    Fix props asssignment.

--- src/components/ParkEvents.js
+++ src/components/ParkEvents.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ParkEvent from './ParkEvent';
 
 const ParkEvents = props => {
-  const { events } = props;
+  const { parkEvents } = props;
 
   console.log("ParkEvents")
   console.log(props)
@@ -10,14 +10,14 @@ const ParkEvents = props => {
 // debugger
 // Fix props/events
 
-  const parkEvents = events.map(obj => <ParkEvent key={obj.id} parkEvent={obj} deleteParkEvent={props.deleteParkEvent} updateParkEvent={props.updateParkEvent} token={props.token} />)
+  const pEvents = parkEvents.map(obj => <ParkEvent key={obj.id} parkEvent={obj} deleteParkEvent={props.deleteParkEvent} updateParkEvent={props.updateParkEvent} token={props.token} />)
 
   if (props.loading) {
     return (<div className="loader"></div>)
   } else {
     return (
       <div className="events">
-        {parkEvents}
+        {pEvents}
       </div>
     )
   }
