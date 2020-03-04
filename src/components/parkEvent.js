ad3685f3b942f0544629987cac128f6fd40cf796
aparkening <aaron.parkening@gmail.com>
2020-03-03 20:30:31 -0500

    Add spacing to event-heading spans.

--- src/components/parkEvent.js
+++ src/components/parkEvent.js
@@ -25,8 +25,8 @@ const ParkEvent = props => {
         <Card className="event mb-4" bg="white" border="coffee">
           <Card.Header as="h5">{parkEvent.summary}</Card.Header>
           <Card.Body>
-            <div className="event-heading"><span>Location:</span> {parkEvent.location} </div>
-            <div className="event-heading"><span>Date:</span> <Moment format="LLL" date={parkEvent.start_time}/> to <Moment format="LLL" date={parkEvent.end_time}/></div>            
+            <div className="event-heading"><span>Location: </span>{parkEvent.location}</div>
+            <div className="event-heading"><span>Date: </span><Moment format="LLL" date={parkEvent.start_time}/> to <Moment format="LLL" date={parkEvent.end_time}/></div>            
             <Card.Text>{parkEvent.description}</Card.Text>
             {/* <Button variant="copper" size="sm" className="mr-2" onClick={() => props.updateEvent(event.id)}> Edit </Button>
             <Button variant="copper" size="sm" className="mr-2" onClick={() => props.deleteEvent(event.id)}> X </Button> */}
