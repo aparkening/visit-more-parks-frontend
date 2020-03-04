820c54d2eb3a49e17bede2b7a2ee035b00da8cdb
aparkening <aaron.parkening@gmail.com>
2020-03-04 11:35:07 -0500

    Add accordian styling to match Park.js.

--- src/components/parkEvent.js
+++ src/components/parkEvent.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import Moment from 'react-moment';
 
-// import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Accordion from 'react-bootstrap/Accordion';
 
 const ParkEvent = props => {
   const { parkEvent } = props;
-  console.log(props);
+  // console.log(props);
 
   if (props.loading) {
     return (<div className="loader"></div>)
   } else {
     return (
-        <Card className="event mb-4" bg="white" border="coffee">
-          <Card.Header as="h5">{parkEvent.summary}</Card.Header>
-          <Card.Body>
-            <div className="event-heading"><span>Location: </span>{parkEvent.location}</div>
-            <div className="event-heading"><span>Date: </span><Moment format="LLL" date={parkEvent.start_time}/> to <Moment format="LLL" date={parkEvent.end_time}/></div>            
+      <Card className="event" bg="white" border="coffee">
+        <Card.Header as="h5">
+          <Accordion.Toggle as={Button} variant="link" eventKey={parkEvent.id}>
+           {parkEvent.summary}
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey={parkEvent.id}>
+          <Card.Body className="mb-4">
+            <div className="event-heading"><img src="icon-location.svg" alt="Location icon" caption="Location by Neha Tyagi from the Noun Project" /> {parkEvent.location}</div>
+            <div className="event-heading"><img src="icon-date.svg" alt="Date icon" caption="date by Karan from the Noun Project" /> <Moment format="LLL" date={parkEvent.start_time}/> to <Moment format="LLL" date={parkEvent.end_time}/></div>            
             <Card.Text>{parkEvent.description}</Card.Text>
             {/* <Button size="sm" className="mr-2" onClick={() => props.updateEvent(event.id)}> Edit </Button>
             <Button size="sm" className="mr-2" onClick={() => props.deleteEvent(event.id)}> X </Button> */}
           </Card.Body>
-        </Card>
+        </Accordion.Collapse>
+      </Card>
     )
   }
 };
