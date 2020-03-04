4805da6982f469dad503413b3ad69d5f20b76424
aparkening <aaron.parkening@gmail.com>
2020-03-04 10:05:47 -0500

    Remove unused Jumbotron.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,6 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
-import Jumbotron from 'react-bootstrap/Jumbotron';
 
 const Welcome = () => (
   <Container className="welcome">
