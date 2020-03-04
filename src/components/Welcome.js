addeab499276b023757f8b971baa6cedb4030a83
aparkening <aaron.parkening@gmail.com>
2020-03-03 22:36:33 -0500

    Style welcome page.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container'
 
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 const Welcome = () => (
-  <Container>
-    <h1>Let's visit more parks!</h1>
-    <p>
-      When you log in with your Google account, Visit More Parks will scan your calendar for upcoming events with locations and suggest nearby U.S. National Parks (within 100 miles).
-    </p>
-    <p>
-      <Button variant="coffee" href="http://localhost:3000/auth/google/redirect">Log in to Google</Button>
-    </p>
+  <Container className="welcome">
+    <Row>
+      <Col xs={6}><img src="/yellowstone-photo.png" alt="Yellowstone park entrance" className="img-fluid" /></Col>
+      <Col>
+        <h1>Want to visit more parks?</h1>
+        <p>
+          Log in with your Google account and we will suggest U.S. National Parks that are nearby your upcoming events.
+        </p>
+        <p>
+          <Button href="http://localhost:3000/auth/google/redirect">Log in with Google</Button>
+        </p>
+      </Col>
+    </Row>
   </Container>
 )
 export default Welcome
