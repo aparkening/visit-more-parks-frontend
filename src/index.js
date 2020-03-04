933948250eed5b94a0f6444087812b3bd2cd2c34
aparkening <aaron.parkening@gmail.com>
2020-03-04 13:15:37 -0500

    Comment out devToolsExtension and imports.

--- src/index.js
+++ src/index.js
@@ -1,35 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
-
-import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware } from 'redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import './custom.scss';
-import './index.css';
-
 import rootReducer from './reducers/index';
 
-// import eventsReducer from './reducers/eventsReducer';
-// const store = createStore(eventsReducer, applyMiddleware(thunk))
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+// import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import './custom.scss';
+// import './index.css';
 
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-
-// import authReducer from './reducers/authReducer';
-// const store = createStore(rootReducer, applyMiddleware(thunk));
 // Bundle middleware with devtools
-const store = createStore(
-  rootReducer, 
-  compose(
-    applyMiddleware(thunk),
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-  )
-);
-// console.log(rootReducer)
+// const store = createStore(
+//   rootReducer, 
+//   compose(
+//     applyMiddleware(thunk),
+//     window.devToolsExtension ? window.devToolsExtension() : f => f
+//   )
+// );
 
 ReactDOM.render(
   <Provider store={store}>
