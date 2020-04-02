cffda1838b2d1c4e8497641798680c62cae6ca46
aparkening <aaron.parkening@gmail.com>
2020-04-02 15:16:55 -0400

    Remove old comments.

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,8 +1,6 @@
 export const authErrored = (state = false, action) => {
   switch (action.type) {
     case 'AUTH_ERRORED':
-      // console.log("Error Action")
-      // console.log(action)
       return action.hasErrored;
     default:
       return state;
@@ -19,22 +17,12 @@ export const authLoading = (state = false, action) => {
   }
 }
 
-// const authReducer = (state = { csrf: "", activeUser: false, loading: false }, action) => {
-
 export const csrf = (state = false, action) => {
-
   switch(action.type) {
     case 'ADD_AUTHORIZATION':
       return action.csrf;
-      // console.log("Adding Authorization")
-      // console.log(action);
-      // return ({csrf: action.csrf, loggedIn: true});
-
     case 'LOG_OUT':
       return action.csrf;
-      // console.log("Logging out")
-      // console.log(action);    
-      // return ({csrf: action.csrf, loggedIn: false});
     default:
       return state;
   }
